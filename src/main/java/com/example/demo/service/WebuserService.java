7addf4e5f7601002b263f9bef6c2681d1180ef06
novolunteeer <novolunteeer@gmail.com>
2026-01-03 16:44:52 +0900

    계정 관리 검색 기능 완성

---
+++ src/main/java/com/example/demo/service/WebuserService.java
@@ -0,0 +1,41 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.WebuserDTO;
+import com.example.demo.mapper.WebuserMapper;
+import com.example.demo.pagination.PageInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class WebuserService {
+    private final WebuserMapper mapper;
+
+    public Map<String,Object> webuserList(int pageNum, String field,
+                                        String keyword){
+        Map<String,Object> map=new HashMap<>();
+        map.put("field",field);
+        map.put("keyword",keyword);
+
+        //전체 사원 수
+        int totalRowCount=mapper.count(map);
+
+        PageInfo pageInfo=new PageInfo(pageNum,5,
+                5,totalRowCount);
+
+        map.put("startRow",pageInfo.getStartRow());
+        map.put("endRow",pageInfo.getEndRow());
+
+        List<WebuserDTO> list=mapper.webuserList(map);
+
+        Map<String,Object> result=new HashMap<>();
+        result.put("list",list);
+        result.put("pageInfo",pageInfo);
+
+        return result;
+    }
+}
