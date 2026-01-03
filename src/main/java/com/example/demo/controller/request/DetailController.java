7b30c3e7e1ae60bacc197f6701b45b07088baca5
novolunteeer <novolunteeer@gmail.com>
2026-01-03 14:22:54 +0900

    콘트롤러 매핑 주소 정리

--- src/main/java/com/example/demo/controller/request/DetailController.java
+++ src/main/java/com/example/demo/controller/request/DetailController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller.request;
 
-import com.example.demo.dto.InboundDetailDTO;
-import com.example.demo.dto.OutboundDetailDTO;
 import com.example.demo.service.RequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,61 +7,48 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @Controller
 @RequiredArgsConstructor
 public class DetailController {
     private final RequestService service;
 
-    @GetMapping("/request/Indetail") //관리자 로그인 시 전체 입고 탭에서 상세보기
-    public String inboundOne(@RequestParam int io_id, Model model){
-            List<InboundDetailDTO> list=service.inboundList(io_id);
-            model.addAttribute("list",list);
+    // ===== 관리자 =====
+
+    @GetMapping("/request/admin/inbound")
+    public String adminInbound(
+            @RequestParam int inbound_id,
+            Model model) {
+
+        model.addAttribute("list", service.inboundList(inbound_id));
         return "request/Indetail";
     }
 
-    @GetMapping("/request/Outdetail") //관리자 로그인 시 전체 탭에서 출고 상세보기
-    public String outboundOne(@RequestParam int io_id, Model model){
-        List<OutboundDetailDTO> list=service.outboundList(io_id);
-        model.addAttribute("list",list);
+    @GetMapping("/request/admin/outbound")
+    public String adminOutbound(
+            @RequestParam int outbound_id,
+            Model model) {
+
+        model.addAttribute("list", service.outboundList(outbound_id));
         return "request/Outdetail";
     }
 
-    @GetMapping("/request/indetail")
-    public String indetail(@RequestParam int inbound_id,Model model){
-        List<InboundDetailDTO> list=service.inboundList(inbound_id);
-        model.addAttribute("list",list);
+    // ===== 사용자 =====
+
+    @GetMapping("/request/user/inbound")
+    public String userInbound(
+            @RequestParam int inbound_id,
+            Model model) {
+
+        model.addAttribute("list", service.inboundList(inbound_id));
         return "request/Indetail1";
     }
 
-    @GetMapping("/request/indetail1")
-    public String indetail1(@RequestParam int io_id,Model model){
-        List<InboundDetailDTO> list=service.inboundList(io_id);
-        model.addAttribute("list",list);
-        return "request/Indetail1";
-    }
+    @GetMapping("/request/user/outbound")
+    public String userOutbound(
+            @RequestParam int outbound_id,
+            Model model) {
 
-    @GetMapping("/request/outdetail")
-    public String outdetail(@RequestParam int outbound_id,Model model){
-        List<OutboundDetailDTO> list=service.outboundList(outbound_id);
-        model.addAttribute("list",list);
+        model.addAttribute("list", service.outboundList(outbound_id));
         return "request/Outdetail1";
-    }
-
-    @GetMapping("/request/outdetail1")
-    public String outdetail1(@RequestParam int io_id,Model model){
-        List<OutboundDetailDTO> list=service.outboundList(io_id);
-        model.addAttribute("list",list);
-        return "request/Outdetail1";
-    }
-
-    @GetMapping("/request/userDetail")
-    public String UserinboundOne(@RequestParam int inbound_id, @RequestParam int webuser_id, Model model){
-        List<InboundDetailDTO> list=service.inboundList(inbound_id);
-        model.addAttribute("list",list);
-        model.addAttribute("inbound_id",inbound_id);
-        model.addAttribute("webuser_id",webuser_id);
-        return "request/userDetail";
     }
 }
